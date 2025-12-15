934499e6cb9230785b393bea91b979f854db0ffb
AnaghRoy <royanagh1999@gmail.com>
2025-12-15 23:15:57 +0530

    Back button

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let allElems = document.querySelectorAll(".elem");
 let allfullElems = document.querySelectorAll(".fullElem");
+let allfullElemsbackBtn = document.querySelectorAll(".fullElem .back");
 
 allElems.forEach(function (elem) {
   elem.addEventListener("click", function () {
     allfullElems[elem.id].style.display = "block";
+  });
+});
+
+allfullElemsbackBtn.forEach(function (back) {
+  back.addEventListener("click", function () {
+    allfullElems[back.id].style.display = "none";
   });
 });
