973d684f040c78f3ee42e9fea738588ca414eade
AnaghRoy <royanagh1999@gmail.com>
2025-12-17 13:17:55 +0530

    Todo-List functions

--- script.js
+++ script.js
@@ -1,15 +1,75 @@
-let allElems = document.querySelectorAll(".elem");
-let allfullElems = document.querySelectorAll(".fullElem");
-let allfullElemsbackBtn = document.querySelectorAll(".fullElem .back");
+function openfeatures() {
+  let allElems = document.querySelectorAll(".elem");
+  let fullElemPage = document.querySelectorAll(".fullElem");
+  let fullElemPagebackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    allfullElems[elem.id].style.display = "block";
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElemPage[elem.id].style.display = "block";
+    });
   });
-});
 
-allfullElemsbackBtn.forEach(function (back) {
-  back.addEventListener("click", function () {
-    allfullElems[back.id].style.display = "none";
+  fullElemPagebackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullElemPage[back.id].style.display = "none";
+    });
   });
-});
+}
+
+openfeatures();
+
+function todoList() {
+  let currentTask = [];
+
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.warn("Task is empty!");
+  }
+
+  function renderTask() {
+    let allTask = document.querySelector(".allTask");
+
+    let sum = "";
+
+    currentTask.forEach(function (elem, index) {
+      sum += `<div class="task">
+    <h5>${elem.task} <span class=${elem.imp}>Imp</span></h5>
+    <button id=${index}>Mark as completed</button>
+    </div>`;
+    });
+
+    allTask.innerHTML = sum;
+
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+
+    document.querySelectorAll(".task button").forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        currentTask.splice(btn.id, 1);
+        renderTask();
+      });
+    });
+  }
+  renderTask();
+
+  let form = document.querySelector(".addTask form");
+  let taskInput = document.querySelector(".addTask form #task-input");
+  let taskDetailsInput = document.querySelector(".addTask form textarea");
+  let taskCheckbox = document.querySelector(".addTask form #check");
+
+  form.addEventListener("submit", function (dets) {
+    dets.preventDefault();
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheckbox.checked,
+    });
+
+    renderTask();
+
+    taskCheckbox.checked = false;
+    taskInput.value = "";
+    taskDetailsInput.value = "";
+  });
+}
+todoList();
